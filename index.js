dbd22cd564d2e92a24d03fb4ade8be9484114753
João Vitor Aguiar <jv.aguiar.sc@gmail.com>
2021-10-17 01:02:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res)=>{
-    return res.json({message:'Server is Up!'});
+    return res.json({message:'Server is Up Modafokas!'});
 })
 
 app.listen(3333);
